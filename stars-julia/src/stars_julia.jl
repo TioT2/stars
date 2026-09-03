356f009a27e3cb11bd56d13a5c932251b195f9fc
TioT2 <piopi4112@gmail.com>
2026-09-04 01:51:58 +0300

    JULIA: Rewritten guards and improved performance

--- stars-julia/src/stars_julia.jl
+++ stars-julia/src/stars_julia.jl
@@ -13,25 +13,22 @@ function from_spherical(phi::T, theta::T)::Vec3{T} where T <: AbstractFloat
     return Vec3{T}(pc * ts, ps * ts, tc)
 end
 
-function dot(l::Vec3{T}, r::Vec3{T})::T where T
-    return l.x * r.x + l.y * r.y + l.z * r.z
-end
-
+dot(l::Vec3{T}, r::Vec3{T}) where T = l.x * r.x + l.y * r.y + l.z * r.z
 Base.:+(l::Vec3{T}, r::Vec3{T}) where T = Vec3(l.x + r.x, l.y + r.y, l.z + r.z)
 Base.:-(v::Vec3{T}) where T = Vec3(-v.x, -v.y, -v.z)
-
-function splitmix64(state::Ref{UInt64})::UInt64
-    r = (state[] += 0x9E3779B97F4A7C15)
-    r = (r ⊻ (r >> 30)) * 0xBF58476D1CE4E5B9
-    r = (r ⊻ (r >> 27)) * 0x94D049BB133111EB
-    return r ⊻ (r >> 31)
-end
 
 mutable struct RandState
     v0::UInt64
     v1::UInt64
     v2::UInt64
     v3::UInt64
+end
+
+function splitmix64(state::Ref{UInt64})::UInt64
+    r = (state[] += 0x9E3779B97F4A7C15)
+    r = (r ⊻ (r >> 30)) * 0xBF58476D1CE4E5B9
+    r = (r ⊻ (r >> 27)) * 0x94D049BB133111EB
+    return r ⊻ (r >> 31)
 end
 
 function RandState(seed::UInt64)::RandState
@@ -59,20 +56,13 @@ function rand_uint64!(xs::RandState)::UInt64
     return result
 end
 
-function rand_unit_float64!(xs::RandState)::Float64
-    return Float64(rand_uint64!(xs)) / Float64(0xFFFF_FFFF_FFFF_FFFF)
-end
+rand_unit_float64!(xs::RandState)::Float64 = Float64(rand_uint64!(xs)) / Float64(0xFFFF_FFFF_FFFF_FFFF)
+rand_unit_float32!(xs::RandState)::Float32 = Float32(rand_unit_float64!(xs))
 
-function rand_unit_float32!(xs::RandState)::Float32
-    return Float32(rand_unit_float64!(xs))
-end
-
-function rand_unit_vec3!(xs::RandState)::Vec3{Float32}
-    return from_spherical(
-        2.0f0 * Float32(Base.pi) * rand_unit_float32!(xs),
-        acos(rand_unit_float32!(xs) * 2.0f0 - 1.0f0)
-    )
-end
+rand_unit_vec3!(xs::RandState)::Vec3{Float32} = from_spherical(
+    2.0f0 * Float32(Base.pi) * rand_unit_float32!(xs),
+    acos(rand_unit_float32!(xs) * 2.0f0 - 1.0f0)
+)
 
 function rand_sphere_vec3!(xs::RandState)::Vec3{Float32}
     while true
@@ -82,9 +72,7 @@ function rand_sphere_vec3!(xs::RandState)::Vec3{Float32}
             rand_unit_float32!(xs) * 2 - 1
         )
 
-        if dot(v, v) <= 1
-            return v
-        end
+        dot(v, v) <= 1 && return v
     end
 end
 
@@ -103,27 +91,11 @@ end
 function TimeCounter()::TimeCounter
     now = SDL_GetPerformanceCounter()
     freq = SDL_GetPerformanceFrequency()
-
-    return TimeCounter(
-        now,
-        now,
-        freq,
-        0.01,
-        0.01,
-        0,
-        freq,
-        now,
-        NaN32
-    )
+    return TimeCounter(now, now, freq, 0.01, 0.01, 0, freq * 3, now, NaN32)
 end
 
-function timer_new_fps(timer::TimeCounter)::Union{Float32, Nothing}
-    return if timer.now == timer.fps_last_measure
-        timer.fps :: Union{Float32, Nothing}
-    else
-        nothing :: Union{Float32, Nothing}
-    end
-end
+timer_new_fps(timer::TimeCounter)::Union{Float32, Nothing} =
+    timer.now == timer.fps_last_measure ? timer.fps : nothing
 
 function timer_update!(timer::TimeCounter)
     dt(t0::UInt64, t1::UInt64)::Float32 = Float32(t1 - t0) / Float32(timer.freq)
@@ -156,22 +128,22 @@ end
 
 function input_from_key(k::SDL_Scancode, pressed::Bool)::Input
     ks = pressed ? +1 : -1
-    return if k == SDL_SCANCODE_Q Input(rot = +ks)
-    elseif    k == SDL_SCANCODE_E Input(rot = -ks)
-    elseif    k == SDL_SCANCODE_R Input(acc = +ks)
-    elseif    k == SDL_SCANCODE_F Input(acc = -ks)
-    elseif    k == SDL_SCANCODE_W Input(dy = +ks)
-    elseif    k == SDL_SCANCODE_S Input(dy = -ks)
-    elseif    k == SDL_SCANCODE_D Input(dx = +ks)
-    elseif    k == SDL_SCANCODE_A Input(dx = -ks)
-    else                          Input()
+    if     k == SDL_SCANCODE_Q Input(rot = +ks)
+    elseif k == SDL_SCANCODE_E Input(rot = -ks)
+    elseif k == SDL_SCANCODE_R Input(acc = +ks)
+    elseif k == SDL_SCANCODE_F Input(acc = -ks)
+    elseif k == SDL_SCANCODE_W Input(dy = +ks)
+    elseif k == SDL_SCANCODE_S Input(dy = -ks)
+    elseif k == SDL_SCANCODE_D Input(dx = +ks)
+    elseif k == SDL_SCANCODE_A Input(dx = -ks)
+    else                       Input()
     end
 end
 
 # Projected star
 struct ProjStar
-    x::Int32
-    y::Int32
+    x::UInt32
+    y::UInt32
     d2::Float32
 end
 
@@ -186,12 +158,10 @@ end
 
 function move_stars!(context::Context, delta::Vec3{Float32})
     for i in eachindex(context.stars)
-        star = context.stars[i] + delta
-        context.stars[i] = if dot(star, star) < 1
-            star
-        else
-            star = rand_unit_vec3!(context.rand)
-            dot(star, delta) >= 0 ? -star : star
+        context.stars[i] += delta
+        if dot(context.stars[i], context.stars[i]) >= 1
+            ns = rand_unit_vec3!(context.rand)
+            context.stars[i] = dot(ns, delta) <= 0 ? ns : -ns
         end
     end
 end
@@ -210,52 +180,41 @@ end
 
 function c_arrow(p::Ptr{T}, s::Symbol) where {T}
     i = findfirst(==(s), fieldnames(T))
-    if i === nothing
-        throw(ArgumentError("$T does not have field $s"))
-    end
+    i === nothing && throw(ArgumentError("$T does not have field $s"))
     return unsafe_load(Ptr{fieldtype(T, i)}(p + fieldoffset(T, i)))
 end
 
 function render!(context::Context)
     surface_ptr::Ptr{SDL_Surface} = SDL_GetWindowSurface(context.window)
+    surface_ptr == C_NULL && return
 
-    if surface_ptr == C_NULL
-        println("Cannot lock window surface: $(unsafe_string(SDL_GetError()))")
-        return
-    end
+    pixel_format_ptr::Ptr{SDL_PixelFormat} = c_arrow(surface_ptr, :format)
+    bpp::UInt8 = c_arrow(pixel_format_ptr, :BytesPerPixel)
+    bpp != 4 && return
 
-    pixel_format_ptr = c_arrow(surface_ptr, :format)
-    bpp = c_arrow(pixel_format_ptr, :BytesPerPixel)
-    if bpp != 4
-        println("Window surface must have BPP of 4")
-        return
-    end
-
-    if SDL_LockSurface(surface_ptr) != 0
-        println("Failed locking window surface: $(unsafe_string(SDL_GetError()))")
-        return
-    end
+    SDL_LockSurface(surface_ptr) != 0 && return
 
     w::Int32 = c_arrow(surface_ptr, :w)
     h::Int32 = c_arrow(surface_ptr, :h)
 
-    whalf::Float32 = w / 2.0f0
-    hhalf::Float32 = h / 2.0f0
-    clip::Float32 = 0.5f0
+    whalf = w / 2.0f0
+    hhalf = h / 2.0f0
+    clip = 0.5f0
     wh_scale = sqrt(Float32(w * w + h * h) / (1 - clip * clip))
-    xymul::Float32 = clip * w * h / wh_scale
+    xymul = clip * w * h / wh_scale
 
     # Project stars and sort them
     proj_stars::Vector{ProjStar} = []
+    sizehint!(proj_stars, length(context.stars) / 2)
     for s::Vec3{Float32} in context.stars
-        if s.z <= 0 continue end
-        xs = Base.unsafe_trunc(Int32, whalf + xymul * s.x / s.z)
-        if xs < 0 || xs >= w - 4 continue end
-        ys = Base.unsafe_trunc(Int32, hhalf - xymul * s.y / s.z)
-        if ys < 0 || ys >= h - 4 continue end
+        s.z <= 0 && continue
+        xs = Base.unsafe_trunc(UInt32, whalf + xymul * s.x / s.z)
+        xs >= w - 4 && continue
+        ys = Base.unsafe_trunc(UInt32, hhalf - xymul * s.y / s.z)
+        ys >= h - 4 && continue
         push!(proj_stars, ProjStar(xs, ys, dot(s, s)))
     end
-    proj_stars = sort(proj_stars, lt = (l, r) -> l.d2 > r.d2)
+    sort!(proj_stars, lt = (l, r) -> l.d2 > r.d2)
 
     pitch::Int32 = c_arrow(surface_ptr, :pitch)
     pixels::Ptr{UInt8} = convert(Ptr{UInt8}, c_arrow(surface_ptr, :pixels))
@@ -312,11 +271,10 @@ function run!(context::Context)
         end
 
         ds = -context.speed * context.timer.delta_time
-        dr = Vec3(context.input.dx * ds, context.input.dy * ds, ds)
-        move_stars!(context, dr)
+        move_stars!(context, Vec3(context.input.dx * ds, context.input.dy * ds, ds))
 
         fps = timer_new_fps(context.timer)
-        if fps isa Float32
+        if fps !== nothing
             println("FPS: $(fps)")
         end
 
@@ -325,13 +283,13 @@ function run!(context::Context)
 end
 
 function (@main)(_)
-    @assert SDL_Init(SDL_INIT_VIDEO) == 0 "SDL2 init failed: $(unsafe_string(SDL_GetError()))"
+    SDL_Init(SDL_INIT_VIDEO) != 0 && return
 
     # Create in 1000x750 to counter usage of system-wide sdl2-compat by other implementations
     window = SDL_CreateWindow("stars-julia", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1000, 750, 0)
     # window = SDL_CreateWindow("stars-julia", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, 0)
 
-    @assert window != C_NULL "Failed to create window"
+    window == C_NULL && return
 
     rand = RandState(UInt64(47))
     stars = [rand_sphere_vec3!(rand) for _ in 1:8192]
